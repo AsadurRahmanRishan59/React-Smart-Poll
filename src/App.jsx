6ec1bedaba09ab8a63d0e7cb5d7ef9d30a5d1588
AsadurRahmanRishan59 <asadur.rahman.rishan@g.bracu.ac.bd>
2024-09-29 23:01:14 +0600

    init

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Container } from 'reactstrap'
+
+
+const App = () =>{
+  return(
+    <Container className="my-3">
+      <h1>Smart Poll</h1>
+    </Container>
+  )
+}
+
+export default App;
