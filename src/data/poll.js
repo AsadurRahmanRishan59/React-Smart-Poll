a142097a6a8704da20ac968001e98ee864d1f824
AsadurRahmanRishan59 <asadur.rahman.rishan@g.bracu.ac.bd>
2024-09-30 15:51:18 +0600

    modal

---
+++ src/data/poll.js
@@ -0,0 +1,47 @@
+const POLLS = [
+    {
+        id: '45184',
+        title: 'What is Your Favorite Programming Language',
+        description:'There are lot of popular programming languages available. Among them what is your favorite?',
+        options:[
+            { id:'14514',value:'C Programming',vote:'0'},
+            { id:'17741',value:'Java',vote:'0'},
+            { id:'41541das',value:'Javascript',vote:'0'},
+            { id:'ssdas444',value:'Python',vote:'0'},
+        ],
+        created: new Date(),
+        totalVote: 0,
+        opinions:[]
+    },
+    {
+        id: '45sd184',
+        title: 'Which Frontend Framework do You Prefer?',
+        description:'javascript has a vast variety of frontend libraries and framework. Each and every day we have a new one. Among those which frontend framework you like and prefer others?',
+        options:[
+            { id:'45621',value:'Angular',vote:'0'},
+            { id:'73489',value:'React',vote:'0'},
+            { id:'41234',value:'Vue',vote:'0'}
+        ],
+        created: new Date(),
+        totalVote: 0,
+        opinions:[]
+    },
+    {
+        id: '354757',
+        title: 'What is the best way to create android app?',
+        description:"I want to create an android application but I don't understand which technology would be better. There are lot of technologies available and my application is not so bulky. I need an easy simple solution, so that I can prototype within no time. Please share your opinions ?",
+        options:[
+            { id:'786345',value:'Java',vote:'0'},
+            { id:'365737',value:'Kotlin',vote:'0'},
+            { id:'9563123',value:'React Native',vote:'0'},
+            { id:'2345643',value:'Flutter',vote:'0'},
+            { id:'566565',value:'Ionic',vote:'0'},
+        ],
+        created: new Date(),
+        totalVote: 0,
+        opinions:[]
+    }
+    
+]
+
+export default POLLS;
